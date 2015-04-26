573bbd31ca9053553753c342bf9b8099a18f55f7
mpenn <matt.w.penn@gmail.com>
2015-04-26 13:55:09 -0700

    added validation

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,2 +1,8 @@
 class Wine < ActiveRecord::Base
+
+  validates :name, :year, :country, :varietal, presence: true
+  validates :year,
+    numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates_inclusion_of :varietal, :in => ['Merlot', 'Chianti', 'Pinot Noir']
+
 end
